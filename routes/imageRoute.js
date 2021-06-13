bd7575c0755ee85ceb811c4cb0d0ac2d919c940d
vishwajeetgade <vishugade15@gmail.com>
2021-06-13 16:36:26 +0530

    readstream added in image route to read images

--- routes/imageRoute.js
+++ routes/imageRoute.js
@@ -91,19 +91,22 @@ router.route("/:id/:user_type/:image_cat")
 					gfs.files.findOne({ filename: newUser.Aadharimg }, (err, file) => {
 						// Check if file
 						if (!file || file.length === 0) {
-						return res.status(404).json({
+						  return res.status(404).json({
 							err: 'No file exists'
-						});
+						  });
 						}
-						if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
-							file.isImage = true;
-						}else{
-							file.isImage = false;
+					
+						// Check if image
+						if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+						  // Read output to browser
+						  const readstream = gfs.createReadStream(file.filename);
+						  readstream.pipe(res);
+						} else {
+						  res.status(404).json({
+							err: 'Not an image'
+						  });
 						}
-						// File exists
-						console.log("File Data: "+ file);
-						return res.json(file);
-					});	
+					  });
 				})
 				.catch(err => res.status(500).json(err))
 			}else if(req.params.user_type === "worker"){
@@ -113,19 +116,22 @@ router.route("/:id/:user_type/:image_cat")
 					gfs.files.findOne({ filename: newUser.Aadharimg }, (err, file) => {
 						// Check if file
 						if (!file || file.length === 0) {
-						return res.status(404).json({
+						  return res.status(404).json({
 							err: 'No file exists'
-						});
+						  });
 						}
-						if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
-							file.isImage = true;
-						}else{
-							file.isImage = false;
+					
+						// Check if image
+						if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+						  // Read output to browser
+						  const readstream = gfs.createReadStream(file.filename);
+						  readstream.pipe(res);
+						} else {
+						  res.status(404).json({
+							err: 'Not an image'
+						  });
 						}
-						// File exists
-						console.log("File Data: "+ file);
-						return res.json(file);
-					});	
+					  });	
 				})
 				.catch(err => res.status(500).json(err))
 			}else if(req.params.user_type === "enterpriceuser"){
@@ -135,19 +141,22 @@ router.route("/:id/:user_type/:image_cat")
 					gfs.files.findOne({ filename: newUser.Aadharimg }, (err, file) => {
 						// Check if file
 						if (!file || file.length === 0) {
-						return res.status(404).json({
+						  return res.status(404).json({
 							err: 'No file exists'
-						});
+						  });
 						}
-						if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
-							file.isImage = true;
-						}else{
-							file.isImage = false;
+					
+						// Check if image
+						if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+						  // Read output to browser
+						  const readstream = gfs.createReadStream(file.filename);
+						  readstream.pipe(res);
+						} else {
+						  res.status(404).json({
+							err: 'Not an image'
+						  });
 						}
-						// File exists
-						console.log("File Data: "+ file);
-						return res.json(file);
-					});	
+					  });	
 				})
 				.catch(err => res.status(500).json(err))
 			}
@@ -184,19 +193,22 @@ router.route("/:id/:user_type/:image_cat")
 					gfs.files.findOne({ filename: newUser.Profileimg }, (err, file) => {
 						// Check if file
 						if (!file || file.length === 0) {
-						return res.status(404).json({
+						  return res.status(404).json({
 							err: 'No file exists'
-						});
+						  });
 						}
-						if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
-							file.isImage = true;
-						}else{
-							file.isImage = false;
+					
+						// Check if image
+						if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+						  // Read output to browser
+						  const readstream = gfs.createReadStream(file.filename);
+						  readstream.pipe(res);
+						} else {
+						  res.status(404).json({
+							err: 'Not an image'
+						  });
 						}
-						// File exists
-						console.log("File Data: "+ file);
-						return res.json(file);
-					});	
+					  });
 				})
 				.catch(err => res.status(500).json(err))
 			}else if(req.params.user_type === "enterpriceuser"){
@@ -206,19 +218,22 @@ router.route("/:id/:user_type/:image_cat")
 					gfs.files.findOne({ filename: newUser.Profileimg }, (err, file) => {
 						// Check if file
 						if (!file || file.length === 0) {
-						return res.status(404).json({
+						  return res.status(404).json({
 							err: 'No file exists'
-						});
+						  });
 						}
-						if(file.contentType === 'image/jpeg' || file.contentType === 'image/png'){
-							file.isImage = true;
-						}else{
-							file.isImage = false;
+					
+						// Check if image
+						if (file.contentType === 'image/jpeg' || file.contentType === 'image/png') {
+						  // Read output to browser
+						  const readstream = gfs.createReadStream(file.filename);
+						  readstream.pipe(res);
+						} else {
+						  res.status(404).json({
+							err: 'Not an image'
+						  });
 						}
-						// File exists
-						console.log("File Data: "+ file);
-						return res.json(file);
-					});	
+					  });	
 				})
 				.catch(err => res.status(500).json(err))
 			}
